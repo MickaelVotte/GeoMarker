2223cbd4b79bd97aa53bb1a8872cabd4958442ff
Mickael Votte <mickael.votte@hotmail.com>
2026-03-24 20:58:33 +0100

    test: add unit tests for User.PasswordUpdate and User.NameUpdate domain methods

---
+++ GeoMarker.Domain.Tests/UserTest/NameUpdate.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using GeoMarker.Domain.Entities;
+using GeoMarker.Domain.Enums;
+
+namespace GeoMarker.Domain.Tests.UserTest
+{
+    public class NameUpdate
+    {
+        [Fact]
+        public void NameUpdate_withValidNames_ShouldUpdateNames()
+        {
+            //Arrange
+            var user = new User("John", "Doe", "johndodo@test.com", "hash", UserRole.User);
+            //Act
+            user.NameUpdate("Jane", "Smith");
+            //Assert
+            user.FirstName.Should().Be("Jane");
+            user.LastName.Should().Be("Smith");
+        }
+
+        [Fact]
+        public void NameUpdate_withNullOrEmptyFirstName_ShouldThrowArgumentException()
+        {
+            //Arrange
+            var user = new User("John", "Doe", "johndodo@test.com", "hash", UserRole.User);
+            //Act
+            Action actNull = () => user.NameUpdate(null, "Smith");
+            Action actEmpty = () => user.NameUpdate("", "Smith");
+            //Assert
+            actNull.Should().Throw<ArgumentException>().WithMessage("First name cannot be null or empty (Parameter 'firstName')");
+            actEmpty.Should().Throw<ArgumentException>().WithMessage("First name cannot be null or empty (Parameter 'firstName')");
+        }
+
+        [Fact]
+        public void NameUpdate_withNullOrEmptyLastName_ShouldThrowArgumentException()
+        {
+            //Arrange
+            var user = new User("John", "Doe", "", "hash", UserRole.User);
+            //Act
+            Action actNull = () => user.NameUpdate("Jane", null);
+            Action actEmpty = () => user.NameUpdate("Jane", "");
+            //Assert
+            actNull.Should().Throw<ArgumentException>().WithMessage("Last name cannot be null or empty (Parameter 'lastName')");
+            actEmpty.Should().Throw<ArgumentException>().WithMessage("Last name cannot be null or empty (Parameter 'lastName')");
+        }
+    }
+}
---
+++ GeoMarker.Domain.Tests/UserTest/PasswordUpdate.cs
@@ -0,0 +1,34 @@
+﻿using GeoMarker.Domain.Entities;
+using GeoMarker.Domain.Enums;
+using FluentAssertions;
+using Xunit;
+
+namespace GeoMarker.Domain.Tests.UserTest
+{
+    public class PasswordUpdate
+    {
+        [Fact]
+        public void PasswordUpdate_withValidPassword_ShouldUpdatePassword()
+        {
+            //Arrange
+            var user = new User("John", "Doe", "", "hash", UserRole.User);
+            //Act
+            user.PasswordUpdate("newHash");
+            //Assert
+            user.PasswordHash.Should().Be("newHash");
+        }
+
+        [Fact]
+        public void PasswordUpdate_withNullOrEmptyPassword_ShouldThrowArgumentException()
+        {
+            //Arrange
+            var user = new User("John", "Doe", "", "hash", UserRole.User);
+            //Act
+            Action actNull = () => user.PasswordUpdate(null);
+            Action actEmpty = () => user.PasswordUpdate("");
+            //Assert
+            actNull.Should().Throw<ArgumentException>().WithMessage("Password hash cannot be null or empty (Parameter 'passwordHash')");
+            actEmpty.Should().Throw<ArgumentException>().WithMessage("Password hash cannot be null or empty (Parameter 'passwordHash')");
+        }
+    }
+}
