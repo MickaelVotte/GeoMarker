9e4e98856e79fd7288ac15d0181d512306700401
Mickael Votte <mickael.votte@hotmail.com>
2026-03-31 21:40:00 +0200

    feat: add AddMember command  validator

--- GeoMarker.Application/Features/Groups/Commands/AddMember/AddMemberCommandValidator.cs
+++ GeoMarker.Application/Features/Groups/Commands/AddMember/AddMemberCommandValidator.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using GeoMarker.Application.Features.Groups.Commands.AddMenber;
+
 
 namespace GeoMarker.Application.Features.Groups.Commands.AddMember
 {
-    internal class AddMemberCommandValidator
+    public sealed class AddMemberCommandValidator : AbstractValidator<AddMemberCommand>
     {
+        public AddMemberCommandValidator()
+        {
+            RuleFor(x => x.OwnerId).NotEmpty().WithMessage("Owner ID cannot be empty");
+            RuleFor(x => x.MemberId).NotEmpty().WithMessage("Member ID cannot be empty");
+            RuleFor(x => x.GroupId).NotEmpty().WithMessage("Group ID cannot be empty");
+        }
     }
 }
